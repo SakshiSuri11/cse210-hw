09082998e8933a1359865576543c12c0b8b5f99b
SakshiSuri11 <sakshisuri444@gmail.com>
2025-05-14 14:45:20 +0300

    week01ex3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        int guess = -1;
+        Console.WriteLine("Please enter a number between 1 and 101. Try to guess it!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("Enter your guess: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low! Try guessing higher.");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high! Try guessing lower.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number: " + magicNumber);
+            }
+        }
     }
 }
